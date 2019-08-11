1af11783aa088e5611ead8c78187f352ed61c707
djd463jjd <54001271+djd463jjd@users.noreply.github.com>
2019-08-11 22:21:11 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -46,7 +46,7 @@ CONFIG_CHECK = os.environ.get("", None)
 
 if CONFIG_CHECK:
     LOGS.error("Please remove the line mentioned in the first hashtag from the config.env file")
-    quit(1)
+    quit(1)os.environ.get("API_KEY","936452")
 
 API_KEY = os.environ.get("API_KEY","936452")
 
